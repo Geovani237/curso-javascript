8bfd2496283fd50e6ef0d93c9500aab50aee6a7f
Geovani237 <94084031+Geovani237@users.noreply.github.com>
2022-10-31 17:22:40 -0300

    Api de Cep
    terminada a api de pegar o cep e encontrar o endereço

--- aula010/app.js
+++ aula010/app.js
@@ -5,20 +5,25 @@ SBO /SP
 */
 
 function BuscarCEP(){
-    let cep = 13455186
+    // let cep = 13455186
+    let cep = $('#pegar-cep').val()
     $.ajax({
         type: "GET",
-        url: " https://cep.awesomeapi.com.br/json/" + cep,
+        url: "https://cep.awesomeapi.com.br/json/" + cep,
         dataType: "json",
         success: function (dados) {
+
             let rua = dados.address
             let bairo = dados.district
-            $('#resultado-pesquisa').html(rua + " " +bairo)
-        console.dir(dados)
-        console.log(dados.address)
-        console.log(dados.district)
-        console.log(dados.city)
-        console.log(dados.state)
+            let cidade = dados.city
+            let estado = dados.state
+            $('#resultado-pesquisa').html(rua + "-" +bairo+ "-" +cidade+ "/" +estado)
+
+        // console.dir(dados)
+        // console.log(dados.address)
+        // console.log(dados.district)
+        // console.log(dados.city)
+        // console.log(dados.state)
         }
     });
 }
